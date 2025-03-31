7ba6e758bf4ac0d02717a085b1fcf6ce54c0a3d3
Saumya-sarkhel <saumyajitsarkhel99@gmail.com>
2025-04-01 02:27:54 +0530

    updated

--- Sorting/Heap-sort.C
+++ Sorting/Heap-sort.C
@@ -3,61 +3,63 @@
 
 void heapify(int *arr, int, int);
 void heapSort(int *arr, int);
-void printArray(int *arr, int);
+void swap(int *a, int *b);
 
 
 int main() {
-    int n, *arr;
+    int n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    arr = (int *)malloc(n * sizeof(int));
-    printf("Enter %d integers:\n", n);
-    for (int i=0; i<n; i++) {
+    int *arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
     heapSort(arr, n);
 
-    printf("Sorted array is \n");
-    printArray(arr, n);
-    return 0;
-}
-
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int l = 2*i + 1;
-    int r = 2*i + 2;
-    if (l<n && arr[l] > arr[largest]) {
-        largest = l;
-    }
-    if (r<n && arr[r] > arr[largest]) {
-        largest = r;
-    }
-    if (largest != i) {
-        int temp = arr[i];
-        arr[i] = arr[largest];
-        arr[largest] = temp;
-        heapify(arr, n, largest);
-    }
-}
-
-void heapSort(int arr[], int n) {
-    for (int i = n/2-1; i>=0; i--) {
-        heapify(arr, n, i);
-    }
-    for (int i=n-1; i>0; i--) {
-
-        int temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
-        heapify(arr, i, 0);
-    }
-}
-
-void printArray(int arr[], int n) {
+    printf("Sorted array:\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
+    free(arr);
+    return 0;
+}
+
+void heapify(int *arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int *arr, int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+    for (int i = n - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
